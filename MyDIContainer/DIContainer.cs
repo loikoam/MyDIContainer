116db9367127e5e299f3e796a657e14a56c43464
loikaam <sanya.loyko.95@mail.ru>
2019-11-17 23:37:34 +0300

    + Add progect MyDIContainer

---
+++ MyDIContainer/DIContainer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDIContainer
+{
+    public static class IoC
+    {
+        static readonly IDictionary<Type, Type> types = new Dictionary<Type, Type>();
+        /// <summary>
+        /// add type to container, generic
+        /// </summary>
+        /// <typeparam name="TContract"></typeparam>
+        /// <typeparam name="TImplementation"></typeparam>
+        public static void Register<TContract, TImplementation>()
+        {
+            types[typeof(TContract)] = typeof(TImplementation);
+        }
+
+        /// <summary>
+        /// add type to container, generic
+        /// </summary>
+        /// <typeparam name="TContract"></typeparam>
+        /// <typeparam name="TImplementation"></typeparam>
+        public static void Register(Type tContract, Type tImplementation)
+        {
+            types[tContract] = tImplementation;
+        }
+
+        /// <summary>
+        /// extract type from container
+        /// </summary>
+        /// <typeparam name="T">Extract type</typeparam>
+        /// <returns></returns>
+        public static T Resolve<T>()
+        {
+            return (T)Resolve(typeof(T));
+        }
+
+        /// <summary>
+        /// extract type from container
+        /// </summary>
+        /// <typeparam name="T">Extract type</typeparam>
+        /// <returns></returns>
+        public static object Resolve(Type contract)
+        {
+            Type implementation = types[contract];
+            ConstructorInfo constructor = implementation.GetConstructors()[0];
+            ParameterInfo[] constructorParameters = constructor.GetParameters();
+            if (constructorParameters.Length == 0)
+            {
+                return Activator.CreateInstance(implementation);
+            }
+
+            List<object> parameters = new List<object>(constructorParameters.Length);
+            foreach (ParameterInfo parameterInfo in constructorParameters)
+            {
+                parameters.Add(Resolve(parameterInfo.ParameterType));
+            }
+
+            return constructor.Invoke(parameters.ToArray());
+        }
+    }
+}
---
+++ UseMyDIContainer/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyDIContainer;
+
+namespace UseMyDIContainer
+{
+    class Program
+    {        
+        public interface ICat
+        {
+            int GetCountPaws();
+        }
+        public class Cat: ICat
+        {
+            public int GetCountPaws()
+            {
+                return 4;
+            }
+        }
+        public interface IDog
+        {
+            string GetBreedName();
+        }
+        public class Dog : IDog
+        {
+            private string _breedName = "Breed";
+            public string GetBreedName()
+            {
+                return _breedName;
+            }
+        }
+
+        public static string Display(IDog dog)
+        {
+            return dog.GetBreedName();
+        }
+
+        public static string Display(ICat cat)
+        {
+            return cat.GetCountPaws().ToString();
+        }
+
+
+        static void Main(string[] args)
+        {
+            IoC.Register<IDog, Dog>();
+            IoC.Register(typeof(ICat), typeof(Cat));
+
+            Console.WriteLine(Display(IoC.Resolve<IDog>()));
+
+            Console.WriteLine(Display((Cat)IoC.Resolve(typeof(ICat))));
+            Console.ReadLine();
+        }
+    }
+}
